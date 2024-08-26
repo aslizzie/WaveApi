582079d5dd213158d642534711a4571291740c1e
Diana Alcala <diana.alcalasandoval@gmail.com>
2024-08-25 23:26:48 -0600

    Proyecto terminado

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Nota importante
+
+El archivo SQL para la base de datos se encuentra dentro de la carpeta dump.
+
+![Descripción de la imagen](/documentation/dump_sql.png)
+
+O bien, se puede consultar en el siguiente enlace:
+[https://drive.google.com/file/d/1wxOCZl-6FIRMsYu3PySKKjFyS-wCstlV/view?usp=sharing](https://drive.google.com/file/d/1wxOCZl-6FIRMsYu3PySKKjFyS-wCstlV/view?usp=sharing).
--- obj/Debug/net8.0/WaveApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WaveApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaveApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f71448ece37a02054a32a7f9e5c8706ede9e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaveApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaveApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
